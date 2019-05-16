4e1752d55a1d791831155800e6c813a1f5c8260d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-16 22:56:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,5 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration
   create_table :landmarks do |t|
     t.string :name
-    t.intege
+    t.integer :figure_id
+    t.integer :year_completed
+  end
 end
